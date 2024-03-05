3f08c4ce3eea61531df9e35fa65158d71f96c7c1
Sameh <sameh.abdelhaleem95@gmail.com>
2024-03-05 11:48:46 +0200

    add Hero Section and hide Nav in small screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,44 @@
+import logo from "../assets/logo.svg";
+
 const Hero = () => {
   return (
     <section id="hero">
       {/* Hero Container */}
-      <div className="container max-w-6xl mx-auto px-6 py-12 md:px-0">
+      <div className="container max-w-6xl mx-auto px-6 py-12 ">
         {/* Menu / Logo container */}
-        <nav className="flex justify-between items-center font-bold text-white"></nav>
+        <nav className="flex justify-between items-center font-bold text-white">
+          {/* logo */}
+          <img src={logo} alt="" />
+          {/* Menu */}
+          <div className="hidden h-10 font-alata md:flex md:space-x-8">
+            <div className="group">
+              <a href="#">About</a>
+              <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+            </div>
+            <div className="group">
+              <a href="#">Careers</a>
+              <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+            </div>
+            <div className="group">
+              <a href="#">Events</a>
+              <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+            </div>
+            <div className="group">
+              <a href="#">Products</a>
+              <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+            </div>
+            <div className="group">
+              <a href="#">Support</a>
+              <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+            </div>
+          </div>
+          {/* @todo Hamburger Button */}
+        </nav>
+        {/* @todo Mobile Menu */}
+
+        <div className="max-w-lg mt-32 mb-32 p-4 font-sans text-4xl text-white uppercase border-2 md:p-10 md:m-32 md:mx-0 md:text-6xl">
+          Impressive Experiences That Deliver
+        </div>
       </div>
     </section>
   );
